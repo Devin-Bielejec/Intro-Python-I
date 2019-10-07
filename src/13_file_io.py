1a2340182859786ca704ff491303360f9a4ea415
devin-bielejec <devin.bieleje@gmail.com>
2019-10-07 12:27:55 -0400

    Completed 13_file_io

--- src/13_file_io.py
+++ src/13_file_io.py
@@ -9,6 +9,9 @@ https://docs.python.org/3/tutorial/inputoutput.html#reading-and-writing-files
 # Print all the contents of the file, then close the file
 
 # YOUR CODE HERE
+f = open("foo.txt", "r")
+print(f.read())
+f.close()
 
 # Open up a file called "bar.txt" (which doesn't exist yet) for
 # writing. Write three lines of arbitrary content to that file,
@@ -16,3 +19,10 @@ https://docs.python.org/3/tutorial/inputoutput.html#reading-and-writing-files
 # sure that it contains what you expect it to contain
 
 # YOUR CODE HERE
+b = open("bar.txt", "w")
+b.write("Chickens are teh best bestse \n apwoeifjpaiefjpaowejifpawiefja wef \n apwoeifjpawejfiopawiejfpaowiejfpoaiwejf")
+b.close()
+
+bb = open("bar.txt", "r")
+
+print(bb.read())
