82b3c64be327e1a6a97a1e25ec3ab3a796ec5f5e
devin-bielejec <devin.bieleje@gmail.com>
2019-10-07 13:02:18 -0400

    Completed 14_cal

--- src/14_cal.py
+++ src/14_cal.py
@@ -6,7 +6,7 @@ https://docs.python.org/3.6/library/calendar.html
 Write a program that accepts user input of the form
   `14_cal.py month [year]`
 and does the following:
- - If the user doesn't specify any input, your program should
+ [X] If the user doesn't specify any input, your program should
    print the calendar for the current month. The 'datetime'
    module may be helpful for this.
  - If the user specifies one argument, assume they passed in a
@@ -22,3 +22,33 @@ and does the following:
 import sys
 import calendar
 from datetime import datetime
+
+def program():
+  userInput = input("File name month year:")
+  inputList = userInput.split(" ")
+
+  if len(inputList) > 3:
+    print("Incorrect form, input should be [file] [month] [year]")
+    return
+
+
+  if userInput == "":
+    m = datetime.today().month
+    y = datetime.today().year
+    print(calendar.month(y, m=m))
+    return
+
+  if len(inputList) > 1:
+    m = inputList[1]
+  else:
+    m = datetime.today().month
+  
+  if len(inputList) > 2:
+    y = inputList[2]
+  else:
+    y = datetime.today().year
+  print(m, y)
+  print(calendar.calendar(int(y), int(m)))  
+
+  
+program()
